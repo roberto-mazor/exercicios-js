5460476abe2f01a5a9b7bb228439fbce3d2beb05
roberto-mazor <mazor.estudo@gmail.com>
2025-09-12 20:42:02 -0300

    Feat: Criado formulário

---
+++ exercicio-003/app.js
@@ -0,0 +1,7 @@
+// 03 Calcular a média de venda à partir dos valores vendidos em cada um dos meses do 1º Trim de 2018 de um vendedor
+let jan = 1500;
+let fev = 2000;
+let mar = 2500;
+
+let media = (jan + fev + mar) / 3;
+console.log(media);
