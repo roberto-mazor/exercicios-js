2f38d4197cced591dee523a24002db9aabaacff9
roberto-mazor <mazor.estudo@gmail.com>
2025-09-12 22:16:30 -0300

    Feat: Concluído exercício 3

--- exercicio-003/app.js
+++ exercicio-003/app.js
@@ -1,7 +1,19 @@
 // 03 Calcular a média de venda à partir dos valores vendidos em cada um dos meses do 1º Trim de 2018 de um vendedor
-let jan = 1500;
-let fev = 2000;
-let mar = 2500;
+// usar formulario HTML para entrada dos dados e exibir o resultado na tela
 
-let media = (jan + fev + mar) / 3;
-console.log(media);
+
+function fnCalcularMedia() {
+    let vendedor = ""; janeiro = 0; fevereiro = 0; marco = 0; media = 0;
+
+    vendedor = document.getElementById("vendedor").value;
+    janeiro = parseFloat(document.getElementById("janeiro").value);
+    fevereiro = parseFloat(document.getElementById("fevereiro").value);
+    marco = parseFloat(document.getElementById("marco").value);
+
+    media = (janeiro + fevereiro + marco) / 3;
+
+    document.getElementById("resultado_media").innerText = media.toFixed(2);
+    return false;
+}
+
+
