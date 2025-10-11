696669bc24c0b33d8f2e2880606044f1e361e790
roberto-mazor <mazor.estudo@gmail.com>
2025-10-10 21:37:19 -0300

    Feat: Array de cores sortidas nos cards

--- app.js
+++ app.js
@@ -1,2 +1,34 @@
-const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
-const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+const colorVars = [
+  '--cinza-escuro',
+  '--grafite',
+  '--cinza-fosco',
+  '--preto-intenso',
+  '--bege-escuro',
+  '--azul-profundo',
+  '--verde-musgo',
+  '--laranja-queimado',
+  '--amarelo-escuro',
+  '--roxo-escuro',
+  '--rosa-antigo',
+  '--azul-fumê',
+  '--verde-musgo-claro',
+  '--lilas-escuro',
+  '--pessego-escuro',
+  '--terracota-escura',
+  '--verde-oliva-escuro',
+  '--marrom-profundo',
+  '--azul-petroleo-escuro',
+  '--verde-floresta-escuro'
+];
+
+
+const randomColorVar = () => colorVars[Math.floor(Math.random() * colorVars.length)];
+
+const corCards = document.querySelectorAll('.corCard');
+
+corCards.forEach(card => {
+  const rootStyles = getComputedStyle(document.documentElement);
+  const colorVar = randomColorVar();
+  const bgColor = rootStyles.getPropertyValue(colorVar);
+  card.style.backgroundColor = bgColor;
+});
